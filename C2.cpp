9ef7e5d076b986fdb285fee48d586bef51d478e8
Hemant524 <rajhemant606@gmail.com>
2025-09-25 17:42:55 +0530

    add C2.cpp

---
+++ C2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// 'int' is a keyword 
+int main() {
+        // 'int' is a keyword 
+    int age = 20;
+    // 'if' is a keyword
+    if (age > 18) { 
+        cout << "Adult";
+    }
+   // 'return' is a keyword 
+    return 0;       
+}
